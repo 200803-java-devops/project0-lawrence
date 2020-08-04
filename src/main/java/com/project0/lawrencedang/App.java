2cc4c0f4c0e5622ba822a25afdab7292027701d3
Lawrence Dang <lawrend1@uci.edu>
2020-08-04 11:36:41 -0700

    Initial commit

---
+++ src/main/java/com/project0/lawrencedang/App.java
@@ -0,0 +1,13 @@
+package com.project0.lawrencedang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
